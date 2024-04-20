034f06da226bf4f41d656f25f98e491f9f95a72a
Tristan Andrus <tristan@tristanandrus.com>
2024-04-20 14:24:39 -0600

    Broken LSP commit
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- init.lua
+++ init.lua
@@ -21,6 +21,58 @@ Plug("nvim-telescope/telescope.nvim")
 vim.call('plug#end')
 
 ----------------------------------------------------
+-- LSP
+----------------------------------------------------
+-- My goal here is simple: I want the best features I can get, while minimizing
+-- the complexity and number of modules, and while maximizing my understanding of
+-- how it works so I can fix it when it breaks. To this end, I use the built-in
+-- Neovim LSP.
+
+-- TODO: These only work if I source this file, i.e. run this code twice.
+--require('vim.lsp')
+
+lsp_attach_callback = function(args)
+    print("lsp attach 2")
+    print("args 2 = ", args)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.hoverProvider then
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+    end
+
+    -- Showing implementation
+    if client.server_capabilities.implementationProvider then
+        -- TODO: See if buffer args are needed.
+        vim.keymap.set('n', 'ti', telescope.lsp_implementations, { buffer = args.buf })
+    end
+    if client.server_capabilities.definitionsProvider then
+        vim.keymap.set('n', 'C-]', telescope.lsp_definitions)
+        vim.keymap.set('n', 'td', telescope.lsp_definitions, { buffer = args.buf })
+    end
+    -- TODO: Wrap these in capabilities checking as welll
+    vim.keymap.set('n', 't<F9>', telescope.lsp_outgoing_calls, { buffer = args.buf })
+    vim.keymap.set('n', 't<F8>', telescope.lsp_incoming_calls, { buffer = args.buf })
+end
+
+-- Callback to set up LSP keybindings, depending on what capabilities are available.
+lsp = require('vim.lsp')
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = lsp_attach_callback })
+
+-- Start LSPs now that our callbacks are attached to the `LspAttach`
+require('vim.lsp.log').set_format_func(vim.inspect)
+if vim.fn.executable('clangd') then
+    print("starting lsp")
+    vim.lsp.start({
+        name = 'clangd-lsp-server',
+        cmd = {'/usr/bin/clangd'},
+        root_dir = "." --TODO: Something clever here
+    })
+    print("started.")
+end
+
+----------------------------------------------------
 -- Text appearence configuration
 ----------------------------------------------------
 -- Enable hybrid numbers
@@ -76,43 +128,4 @@ vim.keymap.set('v', '<leader>gd', telescope.git_bcommits_range, {})
 vim.cmd.colorscheme("codedark")
 
 
-----------------------------------------------------
--- LSP
-----------------------------------------------------
--- My goal here is simple: I want the best features I can get, while minimizing
--- the complexity and number of modules, and while maximizing my understanding of
--- how it works so I can fix it when it breaks. To this end, I use the built-in
--- Neovim LSP.
-if vim.fn.executable('clangd') then
-    vim.lsp.start({
-        name = 'clangd-lsp-server',
-        cmd = {'clangd'},
-        root_dir = "." --TODO: Something clever here
-    })
-end
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-  end,
-})
-
--- Callback to set up LSP keybindings, depending on what capabilities are available.
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client.server_capabilities.hoverProvider then
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-        end
-
-        -- Showing implementation
-        if client.server_capabilities.implementationProvider then
-            -- TODO: See if buffer args are needed.
-            vim.keymap.set('n', 'C-]', telescope.lsp_implementations, { buffer = args.buf })
-            vim.keymap.set('n', '<Leader>i', telescope.lsp_implementations, { buffer = args.buf })
-        end
-        if client.server_capabilities.definitionsProvider then
-            vim.keymap.set('n', '<Leader>d', telescope.lsp_definitions, { buffer = args.buf })
-        end
-    end
-})
