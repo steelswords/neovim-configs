30d94a48219f723256e30e46f3570c65f0d7e198
Tristan Andrus <tristan.andrus@vivint.com>
2026-01-06 16:17:58 -0700

    Avante improvments

--- init.lua
+++ init.lua
@@ -66,18 +66,19 @@ Plug("p00f/clangd_extensions.nvim")
 -- For avante
 Plug('echasnovski/mini.icons')
 Plug('HakonHarnes/img-clip.nvim')
-Plug('zbirenbaum/copilot.lua')
 Plug('stevearc/dressing.nvim') -- for enhanced input UI
 Plug('folke/snacks.nvim') -- for modern input UI
 Plug('MeanderingProgrammer/render-markdown.nvim')
 Plug('MunifTanjim/nui.nvim')
-Plug('yetone/avante.nvim', { ['branch'] = 'master', ['do'] = 'make'})
+Plug('yetone/avante.nvim', { ['branch'] = 'main', ['do'] = 'make'})
 
 
 -- Conditionally install this plugin if hostname is "vivint-laptop"
 local hostname = vim.fn.hostname()
 if hostname == "vivint-laptop" then
-    Plug("github/copilot.vim")
+    -- Plug("github/copilot.vim")
+    Plug('zbirenbaum/copilot.lua')
+    Plug("copilotlsp-nvim/copilot-lsp")
 end
 
 -- Leaving this in here for a bit until I'm sure I don't want it in.
--- lua/llm.lua
+++ lua/llm.lua
@@ -7,47 +7,100 @@
 
 local M = {}
 
-local opt = {
-    url = "http://127.0.0.1:11434",
-    prompts = {
-        Generate_Code = {
-            --prompt = "Given you are working in the following source file: $buf\n\n, please answer as an expert, master programmer who never makes mistakes, always thinks things through thoroughly, and writes clear, bug-free, error-free, clean code, and $input",
-            model = "codellama:7b",
-        }
-    }
-}
+
+
+local function is_work_computer()
+    local hostname = vim.fn.hostname()
+    local work_hostnames = { "vivint-laptop" }
+    return vim.tbl_contains(work_hostnames, hostname)
+end
 
 function M.Get_Ollama_Handle()
-    return require("ollama").setup(opt)
+    local ollama_opts = {
+        url = "http://127.0.0.1:11434",
+        prompts = {
+            Generate_Code = {
+                --prompt = "Given you are working in the following source file: $buf\n\n, please answer as an expert, master programmer who never makes mistakes, always thinks things through thoroughly, and writes clear, bug-free, error-free, clean code, and $input",
+                model = "codellama:7b",
+            }
+        }
+    }
+    return require("ollama").setup(ollama_opts)
 end
 
 M.Get_Ollama_Handle()
 
 require('img-clip').setup({})
-require('copilot').setup({})
+
+-- Copilot
+require('copilot').setup({
+    suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+            accept = "<C-g>",
+            accept_word = false,
+            accept_line = false,
+            next = "<C-Tab>",
+            prev = "<C-S-Tab>",
+        },
+    }
+})
+local function toggle_copilot()
+    local suggestions = require('copilot.suggestion')
+    if suggestions.auto_trigger then
+        print("Copilot suggestions disabled")
+    else
+        print("Copilot suggestions enabled")
+    end
+    suggestions.toggle_auto_trigger()
+end
+vim.keymap.set('n', '<leader>c', toggle_copilot, {})
+
 require('render-markdown').setup({
     opts = {
         file_types = { "markdown", "Avante" },
     },
     ft = { "markdown", "Avante" },
 })
+
+
+-- Providers are dependent on what computer I'm on. Home computers get home models.
+-- Work computers get work models
+local function get_home_or_work_providers()
+    -- Get hostname
+    if is_work_computer() then
+        -- Work providers
+        return {
+            copilot = {},
+            ollama = {
+                -- model = "qwen2.5-coder:3b",
+            },
+
+        }
+    else
+        -- Home providers
+        return {
+            ollama = {
+                model = "qwen2.5-coder:3b",
+            },
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-sonnet-4.5",
+                timeout = 30000, -- Timeout in milliseconds
+                  extra_request_body = {
+                    temperature = 0.75,
+                    max_tokens = 20480,
+                  },
+            },
+        }
+    end
+end
+
 require('avante').setup({
     auto_suggestions_provider = "ollama",
     provider = "ollama",
-    providers = {
-        ollama = {
-            model = "qwen2.5-coder:3b",
-        },
-        -- claude = {
-        --     endpoint = "https://api.anthropic.com",
-        --     model = "claude-sonnet-4.5",
-        --     timeout = 30000, -- Timeout in milliseconds
-        --       extra_request_body = {
-        --         temperature = 0.75,
-        --         max_tokens = 20480,
-        --       },
-        -- },
-    },
+    providers = get_home_or_work_providers(),
     rag_service = {
         enabled = false,
         host_mount = vim.fn.expand("."),
@@ -58,5 +111,3 @@ require('avante').setup({
 })
 
 return M
-
-
